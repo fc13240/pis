588464d3625d8790954d8432e36ac4384b55172d
CaiWei <15256085730@sina.cn>
2016-10-25 15:40:59 +0800

    a

--- src/main/java/zhuanli/controller/NewsController.java
+++ src/main/java/zhuanli/controller/NewsController.java
@@ -2,23 +2,14 @@ package zhuanli.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-<<<<<<< HEAD
-
-import zhuanli.domain.FirstColumn;
-import zhuanli.domain.Page;
-import zhuanli.domain.Patent;
-=======
 import org.springframework.web.bind.annotation.RequestParam;
-
 import zhuanli.domain.News;
->>>>>>> 063b71b22c6cc9a9c5b78eaf37f7e78115905288
 import zhuanli.service.NewsService;
 
 
@@ -32,30 +23,20 @@ public class NewsController {
 	public NewsController(NewsService newsService) {
 		this.newsService = newsService;
 	}
-	
-<<<<<<< HEAD
+
 	@RequestMapping(path="/newsList",method=RequestMethod.GET)
-	public String newsList(Page page, Model model) {
+	public String newsList( Model model) {
+		List<News> news=newsService.getAllNews();
+		model.addAttribute("news", news);
 		return "news_list";
 	}	
 	@RequestMapping(path="/newsPreview")
-	public String newsPreview(Page page, Model model) {
-		return "news_preview";
-	}		
-=======
-	@RequestMapping(path="/getUserNewsById", method=RequestMethod.GET)
-	public String getUserNewsById(@RequestParam("newsId") int newsId,Model model) {
+	public String newsPreview(@RequestParam("newsId") int newsId,Model model) {
 		News news=newsService.getUserNewsById(newsId);
 		model.addAttribute("news", news);
-		return "";
-	}
-	
-	@RequestMapping(path="/getAllNews", method=RequestMethod.GET)
-	public String getAllNews(Model model) {
-		List<News> news=newsService.getAllNews();
-		model.addAttribute("news", news);
-		return "";
-	}
->>>>>>> 063b71b22c6cc9a9c5b78eaf37f7e78115905288
+		return "news_preview";
+	}		
+
+
 	
 }
