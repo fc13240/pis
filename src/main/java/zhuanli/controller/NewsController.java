9622b7a2077e89f8363deef7759aa19d727d3a52
shiqi <591990368@qq.com>
2016-10-25 15:17:18 +0800

    news

--- src/main/java/zhuanli/controller/NewsController.java
+++ src/main/java/zhuanli/controller/NewsController.java
@@ -6,8 +6,13 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import zhuanli.domain.FirstColumn;
+import zhuanli.domain.Page;
+import zhuanli.domain.Patent;
 import zhuanli.service.NewsService;
 
 
@@ -22,7 +27,13 @@ public class NewsController {
 		this.newsService = newsService;
 	}
 	
-	
-	
+	@RequestMapping(path="/newsList",method=RequestMethod.GET)
+	public String newsList(Page page, Model model) {
+		return "news_list";
+	}	
+	@RequestMapping(path="/newsPreview")
+	public String newsPreview(Page page, Model model) {
+		return "news_preview";
+	}		
 	
 }
